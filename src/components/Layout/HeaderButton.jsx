b37ceab52a9cc6ba382bf191a2d4dbe587c0a768
Alisher <zumanovaliser814@gmail.com>
2022-11-01 16:26:20 +0600

    add meal-item-form

--- src/components/Layout/HeaderButton.jsx
+++ src/components/Layout/HeaderButton.jsx
@@ -1,13 +1,14 @@
 import classes from "./HeaderButton.module.css";
-import { CardIcon } from "../Icons/CardIcon";
+import { CartIcon } from "../Icons/CartIcon";
+
 export function HeaderButton() {
   return (
     <button className={classes.button}>
       <span className={classes.icon}>
-        <CardIcon />
+        <CartIcon />
       </span>
       <span>Your Card</span>
-      <span></span>
+      <span className={classes.badge}>3</span>
     </button>
   );
 }
